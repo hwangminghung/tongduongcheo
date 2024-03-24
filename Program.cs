3c45225a1ab43e676a9516307e7d4a1e84db5ace
hwangminghung <hwangminghung@gmail.com>
2024-03-24 15:42:18 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Nhập kích thước của ma trận vuông:");
+        int size = int.Parse(Console.ReadLine());
+
+        double[,] matrix = new double[size, size];
+
+        Console.WriteLine("Nhập các phần tử của ma trận:");
+
+        // Nhập giá trị cho từng phần tử của ma trận
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                Console.WriteLine($"Nhập giá trị cho phần tử [{i}, {j}]:");
+                matrix[i, j] = double.Parse(Console.ReadLine());
+            }
+        }
+
+        double diagonalSum = 0;
+
+        // Tính tổng của các số trong đường chéo chính
+        for (int i = 0; i < size; i++)
+        {
+            diagonalSum += matrix[i, i];
+        }
+
+        Console.WriteLine($"Tổng của các số trong đường chéo chính là {diagonalSum}.");
+    }
+}
